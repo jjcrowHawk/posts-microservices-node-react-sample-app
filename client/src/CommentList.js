42bf78d6667a5bf5772279c41229e3273c14f7df
jjcrow <hawkcrowxx@gmail.com>
2021-03-10 14:13:02 -0500

    implemented query microservice to get posts with their comments. Changed React web app to query data from query microservice

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
+export default ({ comments }) => {
+  //const [comments, setComments] = useState([]);
 
-  const fetchData = async () => {
+  /* const fetchData = async () => {
     const res = await axios.get(
       `http://localhost:4001/posts/${postId}/comments`
     );
 
     setComments(res.data);
-  };
+  };*/
 
-  useEffect(() => {
+  /* useEffect(() => {
     fetchData();
-  }, []);
+  }, []);*/
 
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>;
